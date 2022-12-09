d7a1679fe94a4083d83f541199f5f67849e18346
gramcha <gramcha@pando.in>
2022-12-09 14:10:06 +0530

    adding node18 and nginx 1.21.6

--- buildv2.sh
+++ buildv2.sh
@@ -1,3 +1,3 @@
-version=2.0.0
-docker build --tag gramcha/alpine-node-nginx:$version -f Dockerfile-node18-nginx1.21.6 .
+version=2.0.1
+docker buildx build --push --tag gramcha/alpine-node-nginx:$version -f Dockerfile-node18-nginx1.21.6 -o type=image --platform=linux/arm64,linux/amd64 .
 docker scan gramcha/alpine-node-nginx:$version
